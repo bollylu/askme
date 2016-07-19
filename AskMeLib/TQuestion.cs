54896b95b0907903cd7f09b6fed4c4fe29aec163
bollylu <lbolly@sharenet.be>
2016-07-19 09:39:07 +0200

    Updated with previous code

---
+++ AskMeLib/TQuestion.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AskMeLib {
+  public class TQuestion {
+
+    public string Title { get; set; }
+    public List<string> Choices { get; set; }
+    public int CorrectAnswer { get; set; }
+    public int CurrentChoice { get; set; }
+
+    public TQuestion() {
+      Title = "";
+      Choices = new List<string>();
+    }
+    public TQuestion(string title) : this() {
+      Title = title;
+    }
+    public TQuestion(string title, IEnumerable<string> choices, int correctAnswer) : this() {
+      Title = title;
+      CorrectAnswer = correctAnswer;
+      foreach (string ChoiceItem in choices) {
+        Choices.Add(ChoiceItem);
+      }
+    }
+
+    public bool Ask() {
+      bool ReponseOk = false;
+
+      // Display the question and get a valid answer
+      do {
+        Console.WriteLine(Title);
+        int i = 1;
+        foreach (string ChoixItem in Choices) {
+          Console.WriteLine(string.Format("  {0}. {1}", i++, ChoixItem));
+        }
+        Console.WriteLine("Veuillez choisir une des valeurs proposées");
+
+        try {
+          CurrentChoice = int.Parse(Console.ReadLine());
+          if (CurrentChoice > 0 && CurrentChoice < i) {
+            ReponseOk = true;
+          }
+        } catch (Exception ex) {
+          Console.WriteLine(string.Format("Erreur : {0}", ex.Message));
+        }
+      } while (!ReponseOk);
+
+      // Test the answer for correctness
+      if (CurrentChoice == CorrectAnswer) {
+        return true;
+      } else {
+        return false;
+      }
+
+
+    }
+  }
+}
--- AskMeTestConsole/Program.cs
+++ AskMeTestConsole/Program.cs
@@ -3,10 +3,55 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BLTools.ConsoleExtension;
+using AskMeLib;
 
 namespace AskMeTestConsole {
   class Program {
     static void Main(string[] args) {
+      int Counter = 0;
+
+      const string QHenri = "Quelle est la couleur du cheval blanc d'Henri IV";
+      List<string> HenriCheval = new List<string>() {
+        "blanc",
+        "noir",
+        "vert",
+        "bleu"
+      };
+      const string QSang = "Quelle est la proportion de globules rouges/globules blancs/plaquettes dans le sang ?";
+      List<string> QBloodAnswer = new List<string>() {
+        "10%, 60%, 30%",
+        "70%, 25%, 5%",
+        "42%, 55%, 3%",
+        "1%, 1%, 98%"
+      };
+
+      TQuestion Question1 = new TQuestion();
+      TQuestion Question2 = new TQuestion(QHenri);
+      TQuestion Question3 = new TQuestion(QHenri, HenriCheval, 1);
+
+      if (Question3.Ask() == true) {
+        Console.WriteLine("La réponse est correcte");
+        Counter++;
+      } else {
+        Console.WriteLine("Perdu !");
+      }
+      ConsoleExtension.Pause();
+      Console.WriteLine();
+
+      TQuestion Question4 = new TQuestion(QSang, QBloodAnswer, 2);
+
+      if (Question4.Ask() == true) {
+        Console.WriteLine("La réponse est correcte");
+        Counter++;
+      } else {
+        Console.WriteLine("Perdu !");
+      }
+      ConsoleExtension.Pause();
+
+      Console.WriteLine();
+      Console.WriteLine(string.Format("Vous avez réussi {0} question(s), votre pourcentage est de {1}%", Counter, Counter / 2f * 100f));
+      ConsoleExtension.Pause();
     }
   }
 }
